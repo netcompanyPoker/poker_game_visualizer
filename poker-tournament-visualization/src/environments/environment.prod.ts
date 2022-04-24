3bcbe4343dfde7e409a74e538a8df8f97d0a7068
Troels Krøgh <troels.kroegh@gmail.com>
2022-04-24 21:27:23 +0200

    prod config

--- poker-tournament-visualization/src/environments/environment.prod.ts
+++ poker-tournament-visualization/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  // wsEndpoint: 'ws://pokerws.satai.dk',
+  wsEndpoint: 'wss://pokerws.satai.dk',
+  reconnectInterval: 2000
 };
